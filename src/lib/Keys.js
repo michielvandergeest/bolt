bd4c825398e8eab90f53ee25c7969ef8f4192c47
Michiel van der Geest <hello@michielvandergeest.com>
2019-10-08 21:52:03 -0300

    Added support for capture keys (besides handle keys).

--- src/lib/Keys.js
+++ src/lib/Keys.js
@@ -1,32 +1,75 @@
 export default SuperClass => {
-  // todo: add support for capture as well
+  // by default keys are considered to be of the type `handle`
+  // capture keys can be configured in a `capture` object inside `keys`
+  // optionally (to be more explicit) handle keys can also be wrapped
+  // inside a `handle` object inside `keys` (which takes presedence over root keys!)
   return class BoltKeys extends SuperClass {
     _handleLeft() {
-      return this.exec(this.config.keys.left)
+      return this.exec(
+        (this.config.keys.handle && this.config.keys.handle.left) || this.config.keys.left
+      )
     }
 
     _handleRight() {
-      return this.exec(this.config.keys.right)
+      return this.exec(
+        (this.config.keys.handle && this.config.keys.handle.right) || this.config.keys.right
+      )
     }
 
     _handleUp() {
-      return this.exec(this.config.keys.up)
+      return this.exec(
+        (this.config.keys.handle && this.config.keys.handle.up) || this.config.keys.up
+      )
     }
 
     _handleDown() {
-      return this.exec(this.config.keys.down)
+      return this.exec(
+        (this.config.keys.handle && this.config.keys.handle.down) || this.config.keys.down
+      )
     }
 
     _handleEnter() {
-      return this.exec(this.config.keys.enter)
+      return this.exec(
+        (this.config.keys.handle && this.config.keys.handle.enter) || this.config.keys.enter
+      )
     }
 
     _handleBack() {
-      return this.exec(this.config.keys.back)
+      return this.exec(
+        (this.config.keys.handle && this.config.keys.handle.back) || this.config.keys.back
+      )
     }
 
     _handleExit() {
       return this.exec(this.config.keys.exit)
+    }
+
+    _captureLeft() {
+      return this.exec(this.config.keys.capture && this.config.keys.capture.left)
+    }
+
+    _captureRight() {
+      return this.exec(this.config.keys.capture && this.config.keys.capture.right)
+    }
+
+    _captureUp() {
+      return this.exec(this.config.keys.capture && this.config.keys.capture.up)
+    }
+
+    _captureDown() {
+      return this.exec(this.config.keys.capture && this.config.keys.capture.down)
+    }
+
+    _captureEnter() {
+      return this.exec(this.config.keys.capture && this.config.keys.capture.enter)
+    }
+
+    _captureBack() {
+      return this.exec(this.config.keys.capture && this.config.keys.capture.back)
+    }
+
+    _captureExit() {
+      return this.exec(this.config.keys.capture && this.config.keys.capture.exit)
     }
   }
 }
